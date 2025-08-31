13089bdbc0156b00a0e3ac7ee20f01eab3d6fbf2
Prashanth Kumar <prashantht9527@gmail.com>
2025-08-31 18:01:20 +0530

    Information

--- node_modules/frontend/src/components/StudentLayout.tsx
+++ node_modules/frontend/src/components/StudentLayout.tsx
@@ -58,6 +58,15 @@ const menuItems = [
     ),
   },
   {
+    name: 'Information',
+    href: '/student/information',
+    icon: (
+      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+    ),
+  },
+  {
     name: 'Subscriptions',
     href: '/student/subscriptions',
     icon: (
