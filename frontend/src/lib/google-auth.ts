5262fe41f2eddee6f6b1d1811f315a372f13e123
Prashanth Kumar <prashantht9527@gmail.com>
2025-09-06 00:00:51 +0530

    Google login changes

--- frontend/src/lib/google-auth.ts
+++ frontend/src/lib/google-auth.ts
@@ -10,7 +10,7 @@ export const GOOGLE_SCOPES = [
 ].join(' ');
 
 // Google OAuth endpoints
-export const GOOGLE_AUTH_URL = 'https://accounts.google.com/oauth/authorize';
+export const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 export const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
 export const GOOGLE_USER_INFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo';
 
