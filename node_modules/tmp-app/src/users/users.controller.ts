91f1bba0606dc6f97fab1dbd094f5b2bde50e1bb
Prashanth Kumar <prashantht9527@gmail.com>
2025-09-07 20:20:53 +0530

    CORS issue

--- node_modules/tmp-app/src/users/users.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Req } from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {
-	@Get('me')
-	me(@Req() req: any) {
-		return req.user || null;
-	}
-} 
